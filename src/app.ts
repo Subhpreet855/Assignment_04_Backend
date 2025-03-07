a9e86f5601b06c1b6f95024095b2abfa28b78af3
Subhpreet Singh <subhpreetsingh657@gmail.com>
2025-03-06 20:38:58 -0600

    Defining a app.ts file for the routes and swagger.

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import setupSwagger from "../config/swagger";   
+import errorHandler from "../src/api/v1/middleware/errorHandler";
+import loanRoutes from "./api/v1/routes/loanRoutes";
+
+const app = express();
+
+// Use Morgan for HTTP request logging
+app.use(morgan("combined"));
+
+app.use(express.json()); 
+
+// Health check endpoint
+app.get("/health", (req, res) => {
+  res.send("Server is healthy");
+});
+
+// Use the loanRoutes for loan endpoints
+app.use("/api/v1/loans", loanRoutes); 
+
+// Set up Swagger for API documentation
+setupSwagger(app);
+
+/*
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+*/
+app.use(errorHandler);
+
+export default app;
