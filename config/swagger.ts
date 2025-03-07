b2e41935416eeb6c383328acc5ab0e6f5080d02c
Subhpreet Singh <subhpreetsingh657@gmail.com>
2025-03-06 23:21:36 -0600

    Make a file swagger.ts for the proper functioning in the swagger.

---
+++ config/swagger.ts
@@ -0,0 +1,33 @@
+// import swagger ui middles, jsdoc-library
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+ 
+import {Express} from "express"
+ 
+ 
+// define swagger options
+const swaggerOptions: swaggerJsDoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API Documention",
+            version: "1.0.0",
+            description: "This is the API documentation for the server"
+        },
+ 
+    },
+    // path to annotated files
+    apis: ["./src/api/v1/routes/loanRoutes.ts",
+          "./src/app.ts",
+        ],
+};
+// Intialize Swagger JSDoc object
+const swaggerDocs: any = swaggerJsDoc(swaggerOptions);
+ 
+// serve swagger
+const setSwagger = (app: Express): void => {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+}
+ 
+// export swagger endpoint for Express app
+export default setSwagger;
